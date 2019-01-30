c2e6185eb903a86a734457c425d128248c13dcd9
Tom Holmes <thomasjj.holmes@gmail.com>
2019-01-30 16:28:23 +0000

    added clock and media queries for responsiveness

--- js/scripts.js
+++ js/scripts.js
@@ -209,3 +209,15 @@ function buildIWContent(place) {
         document.getElementById('iw-website-row').style.display = 'none';
     }
 }
+
+
+
+
+
+
+window.setInterval(ut, 1000);
+
+function ut() {
+  var d = new Date();
+  document.getElementById("time").innerHTML = d.toLocaleTimeString();
+}
